3dbcf4e9ab777ffba9b279a140b0099e5b4fd1d0
margocat57 <bin242717@gmail.com>
2025-10-19 16:23:32 +0300

    new function in assembler fill_listing_struct

--- assembler_task/parsing_str.cpp
+++ assembler_task/parsing_str.cpp
@@ -13,6 +13,8 @@ static void push(int cmd, assembler* assembl);
 
 static void other(int cmd, assembler* assembl);
 
+static void put_params(listing* info, assembler* assembl, long long int pc_in_bytecode_arr, int idx);
+
 static char* skip_space(char* current_str){
     char* str_without_space = current_str;
     str_without_space = str_without_space + strspn(current_str, " \t\n\r\f\v");
@@ -24,6 +26,7 @@ listing* fill_listing_struct(assembler* assembl){
     if(!info){
         return NULL;
     }
+    long long int no_cmd = -1;
 
     for (size_t idx = 0; idx < assembl->file_in_arr.amount_str; idx++){
         for(size_t cmd = 1; cmd < AMNT_CMD; cmd++){
@@ -34,26 +37,30 @@ listing* fill_listing_struct(assembler* assembl){
             // В отдельную функцию, она будет простой
             if(COMANDS[cmd].num_of_params >= 1 &&
                 !strncmp(assembl->ptr_array[idx], COMANDS[cmd].name_of_comand, COMANDS[cmd].size)){
-                (info + idx)->instruction = assembl->ptr_array[idx];
-                (info + idx)->pc = assembl->asm_bytecode_size;
+                put_params(info, assembl, assembl->asm_bytecode_size, idx);
                 (assembl->asm_bytecode_size) += 2;
                 break;
             }
             else if(!strncmp(assembl->ptr_array[idx], COMANDS[cmd].name_of_comand, COMANDS[cmd].size)){
-                (info + idx)->instruction = assembl->ptr_array[idx];
-                (info + idx)->pc = assembl->asm_bytecode_size;
+                put_params(info, assembl, assembl->asm_bytecode_size, idx);
                 (assembl->asm_bytecode_size)++;
                 break;
             }
             else if(!strncmp(assembl->ptr_array[idx], ":", 1)){
-                (info + idx)->instruction = assembl->ptr_array[idx];
-                (info + idx)->pc = -1;
+                put_params(info, assembl, no_cmd, idx);
                 break;
             }
         }
     }
     return info;
 }
+
+static void put_params(listing* info, assembler* assembl, long long int pc_in_bytecode_arr, int idx){
+    (info + idx)->instruction = assembl->ptr_array[idx];
+    (info + idx)->pc = pc_in_bytecode_arr;
+}
+
+
 
 //  Зачем возвращать указатель на структуру, ты её и так меняешь по указателю
 // Лучше возвращать ошибку!
